7728274e354fe74afe42e7b421f7e781d78fdfc2
HungryWinter <github_winter@163.com>
2018-03-06 14:30:57 +0800

    杏冬系统起步

---
+++ src/main/java/com/song/ddemo/DdemoApplication.java
@@ -0,0 +1,12 @@
+package com.song.ddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdemoApplication.class, args);
+	}
+}
